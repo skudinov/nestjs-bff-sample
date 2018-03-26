61b9db7335568e90894a0610c4a313c90ff4e9f0
Viachaslau_Kudzinau <Viachaslau_Kudzinau@epam.com>
2018-03-26 16:38:29 +0200

    Added components mocks and tickets BFF end-point

--- src/modules/tickets/tickets.service.ts
+++ src/modules/tickets/tickets.service.ts
@@ -1,14 +1,26 @@
 import {Component} from '@nestjs/common';
 import {Observable} from 'rxjs/Observable';
 import 'rxjs/add/observable/of';
+import 'rxjs/add/observable/fromPromise';
 import {Ticket} from './tickets.model';
+import {setup, fetch, Response} from 'fetch-h2';
 
 @Component()
 export class TicketsService {
   constructor() {
+    setup({
+      session: {
+        rejectUnauthorized: false
+      }
+    });
   }
 
   findAll(): Observable<Ticket[]> {
-    return Observable.of([{id: 'id1'}]);
+    // HTTP/2 client
+    const result: Promise<Ticket[]> = fetch('https://localhost:3100/tickets')
+      .then((response: Response) => {
+        return response.json();
+      });
+    return Observable.fromPromise(result);
   }
 }
