2f4dd14c8e69e232e264710688ffd15290bed43d
Viachaslau_Kudzinau <Viachaslau_Kudzinau@epam.com>
2018-03-27 12:03:09 +0200

    Added jasmine and istanbul

--- src/modules/tickets/tickets.controller.spec.ts
+++ src/modules/tickets/tickets.controller.spec.ts
@@ -2,14 +2,16 @@ import { Test } from '@nestjs/testing';
 import { TestingModule } from '@nestjs/testing/testing-module';
 import { TicketsController } from './tickets.controller';
 import { expect } from 'chai';
+import {TicketsService} from './tickets.service';
+import {TicketsFactory} from './tickets.factory';
+import {ComponentsService} from '../components';
 
 describe('TicketsController', () => {
   let module: TestingModule;
   beforeEach(() => {
     return Test.createTestingModule({
-      controllers: [
-        TicketsController
-      ]
+      controllers: [TicketsController],
+      components: [TicketsFactory, TicketsService, ComponentsService]
     }).compile()
       .then(compiledModule => module = compiledModule);
   });
