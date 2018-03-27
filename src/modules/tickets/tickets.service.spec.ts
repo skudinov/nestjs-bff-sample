8d034e87b2eacee4ac157a783cd2378b6ffb9939
Viachaslau_Kudzinau <Viachaslau_Kudzinau@epam.com>
2018-03-27 16:40:36 +0200

    Minor code style changes

--- src/modules/tickets/tickets.service.spec.ts
+++ src/modules/tickets/tickets.service.spec.ts
@@ -1,18 +1,9 @@
+import {fetch_h2_mock} from '../../mocks';
 import {Test} from '@nestjs/testing';
 import {TestingModule} from '@nestjs/testing/testing-module';
-import {fetch_h2_mock} from '../../mocks';
 import {TicketsService} from './tickets.service';
 
 describe('TicketsService', () => {
-
-  beforeEach(() => {
-//    mockery.enable();
-  });
-
-  afterEach(() => {
-//    mockery.disable();
-  });
-
   let module: TestingModule;
   beforeEach(() => {
     return Test.createTestingModule({
